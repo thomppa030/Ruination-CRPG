5dc34019688fee19b5990723e4206bc9cefa469a
Kay Priesnitz <kpriesnitz@outlook.de>
2023-04-06 03:00:26 +0200

    Initialize Git LFS

---
+++ Source/CRPGRuination/CRPGRuinationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CRPGRuinationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRPGRUINATION_API ACRPGRuinationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
