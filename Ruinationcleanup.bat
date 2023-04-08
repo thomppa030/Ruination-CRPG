911cf5e8d7dac39904634adfd154bd7300d944f5
Kay Priesnitz <kpriesnitz@outlook.de>
2023-04-08 12:34:58 +0200

    ADD Script for fetching Submodules

---
+++ Ruinationcleanup.bat
@@ -0,0 +1,39 @@
+echo off
+
+del *.sln
+rmdir /s /q .vs
+rmdir /s /q Binaries
+rmdir /s /q Intermediate
+rem rmdir /s /q Saved
+rmdir /s /q DerivedDataCache
+
+rmdir /s /q Plugins\System\ModularClassFramework\Binaries
+rmdir /s /q Plugins\System\ModularClassFramework\Intermediate
+
+rmdir /s /q Plugins\EditorAssetActions\Binaries
+rmdir /s /q Plugins\EditorAssetActions\Intermediate
+
+rmdir /s /q Plugins\GASGameplayFeatureActions\Binaries
+rmdir /s /q Plugins\GASGameplayFeatureActions\Intermediate
+
+rmdir /s /q Plugins\GASFramework\Binaries
+rmdir /s /q Plugins\GASFramework\Intermediate
+
+rmdir /s /q Plugins\GameFeatures\SimpleJumpGAS\Binaries
+rmdir /s /q Plugins\GameFeatures\SimpleJumpGAS\Intermediate
+
+set MyUVS="C:\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win64\UnrealVersionSelector.exe"
+
+set MyUBT="R:\Unreal Engine\UE_5.1\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe"
+
+rem change Transformation to your own project name
+set MyFullPath="%cd%\CRPGRuination"
+
+
+%MyUVS% /projectfiles %MyFullPath%.uproject
+
+%MyUBT% Development Win64 -Project=%MyFullPath%.uproject -TargetType=Editor -Progress -NoEngineChanges -NoHotReloadFromIDE
+
+%MyFullPath%.uproject
+
+%MyFullPath%.sln
